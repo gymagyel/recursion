3f6fac1fea139100f4d6e54c28487ca75bcb4d5a
gymagyel <227876535+gymagyel@users.noreply.github.com>
2026-05-06 13:01:08 +0100

    Add merge sort implementation

---
+++ mergeSort.js
@@ -0,0 +1,34 @@
+function mergeSort(arr) {
+    if (arr.length <=1) return arr;
+  
+    const mid = Math.floor(arr.length / 2);
+const left = arr.slice(0, mid);
+const right = arr.slice(mid);
+
+return merge(
+    mergeSort(left),
+    mergeSort(right)
+);
+function merge(left, right) {
+  const result = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  // add remaining elements
+  return result
+    .concat(left.slice(i))
+    .concat(right.slice(j));
+}
+}
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
